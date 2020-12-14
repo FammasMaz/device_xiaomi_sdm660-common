ed9737718cbc86b98abc64f3d5aaa6854219c170
Moeeze Hassan <fammas.maz@gmail.com>
2020-12-14 18:25:00 +0500

    Revert "Update sdm660.mk"
    This reverts commit 4b762def1fdd24b90c1ab7d1e78cec78a966e3cc.

--- sdm660.mk
+++ sdm660.mk
@@ -132,6 +132,8 @@ PRODUCT_PACKAGES += \
     libantradio
 
 # ANT Permission
+PRODUCT_COPY_FILES += \
+    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
 # AuthSecret
 PRODUCT_PACKAGES += \
